2439890333c36fe223f9041ff56ccd111e043245
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-04-07 11:48:15 -0500

    'link'

--- VideosWebhook/index.js
+++ VideosWebhook/index.js
@@ -25,7 +25,7 @@ server.post('/getVideoDetails', (req, res) => {
         responseFromAPI.on('end', () => {
             const videoDetails = JSON.parse(completeResponse);
             let link = 'https://www.youtube.com/watch?v=';
-            let dataToSend = link + videoDetails.items[0].id.videoId + ",  " + link + videoDetails.items[1].id.videoId + ",  " + link + videoDetails.items[2].id.videoId;
+            let dataToSend = link + videoDetails.items[0].id.videoId + "," + link + videoDetails.items[1].id.videoId + "," + link + videoDetails.items[2].id.videoId;
             // var speechResponse = {
             //   google: {
             //     expectUserResponse: true,
