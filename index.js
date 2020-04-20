0966281b38c5003d2b9a666a3ac8c05e682dc96c
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-04-19 20:25:07 -0500

    'urls'

--- index.js
+++ index.js
@@ -20,58 +20,11 @@ server.post('/web-hook', function (req, response, next) {
     const query = req.body.queryResult.queryText;
     const videoUrl = encodeURI(`https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&type=video&maxResults=3&order=relevance&relevanceLanguage=en&q=${query}&key=${apiKey}`);
     const linkUrl = encodeURI(`https://www.googleapis.com/customsearch/v1?&key=${apiKey}&cx=017576662512468239146:omuauf_lfve&q=${query}&num=3&hl=en`);
-    // axios.all([
-    //     axios.get(linkUrl),
-    //     axios.get(videoUrl)
-    // ]).then(axios.spread((linkRes, videoRes) => {
-    //     console.log(linkRes.items[0].link);
-    //     let linkResponse = '';
-    //     linkRes.on('data', (chunk) => {
-    //         linkResponse += chunk;
-    //     });
-    //     linkRes.on('end', () => {
-    //         const linkDetails = JSON.parse(linkResponse);
-    //         linksData = linkDetails.items[0].link + "," + linkDetails.items[0].link + "," + linkDetails.items[0].link;
-    //         console.log('--------------------------linkdetails');
-    //         console.log(linkDetails);
-    //     });
-    //     console.log('-----------------------------linkdata');
-    //     console.log(linksData);
-    //     let videoResponse = '';
-    //     videoRes.on('data', (chunk) => {
-    //         videoResponse += chunk;
-    //     });
-    //     videoRes.on('end', () => {
-    //         const videoDetails = JSON.parse(videoResponse);
-    //         let link = 'https://www.youtube.com/embed/';
-    //         videoData = link + videoDetails.items[0].id.videoId + "," + link + videoDetails.items[1].id.videoId + "," + link + videoDetails.items[2].id.videoId;
-    //     });
-    //     console.log('---------------------------------------');
-    //     console.log(linksData, videoData);
-    //     console.log('---------------------------------------');
-    //
-    //     return response.json({
-    //         fulfillmentText: linksData + ',' + videoData,
-    //         speech: linksData + ',' + videoData,
-    //         source: 'get-webhook-details'
-    //     });
-    // }));
-
-    // axios.get(linkUrl)
-    //     .then(function (linkRes) {
-    //         console.log('+++++++++++++++++++++++++++=',linkRes);
-    //     })
-    //     .get(videoUrl)
-    //     .then(function (videoRes) {
-    //         console.log('----------------------'+videoRes);
-    //     })
-    //     .catch(function (e) {
-    //
-    //     })
 
     axios.all([
         axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&type=video&maxResults=3&order=relevance&relevanceLanguage=en&q=${query}&key=${apiKey}`),
-        axios.get(`https://www.googleapis.com/customsearch/v1?&key=${apiKey}&cx=017576662512468239146:omuauf_lfve&q=${query}&num=3&hl=en`)
+        // axios.get(`https://www.googleapis.com/customsearch/v1?&key=${apiKey}&cx=017576662512468239146:omuauf_lfve&q=${query}&num=3&hl=en`)
+        axios.get(linkUrl)
     ])
         .then(axios.spread((videoRes, linkRes) => {
             // do something with both responses
