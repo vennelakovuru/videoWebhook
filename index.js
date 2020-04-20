7cd18c17096a78f47c9d5613166dcd0910ea89df
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-04-19 20:18:01 -0500

    'urls'

--- index.js
+++ index.js
@@ -75,21 +75,21 @@ server.post('/web-hook', function (req, response, next) {
     ])
         .then(axios.spread((videoRes, linkRes) => {
             // do something with both responses
-            console.log('+++++++++++++++++++++++++++');
             const linkResponse = JSON.stringify(linkRes.data)
             const linkDetails = JSON.parse(linkResponse);
             linksData = linkDetails.items[0].link + "," + linkDetails.items[0].link + "," + linkDetails.items[0].link;
             console.log(linksData);
-            console.log('-------------------');
             const videoResponse = JSON.stringify(videoRes.data)
             const videoDetails = JSON.parse(videoResponse);
             let link = 'https://www.youtube.com/embed/';
             videoData = link + videoDetails.items[0].id.videoId + "," + link + videoDetails.items[1].id.videoId + "," + link + videoDetails.items[2].id.videoId;
             console.log(videoData);
+            return response.json({
+                fulfillmentText: linksData + ',' + videoData,
+                speech: linksData + ',' + videoData,
+                source: 'get-webhook-details'
+            });
         }));
-
-
-
 });
 
 server.listen((process.env.PORT || 8333), () => {
