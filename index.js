26cd020b4e9b7db343682b319de0639f94b67aa1
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-04-27 02:43:19 -0500

    'links'

--- index.js
+++ index.js
@@ -31,15 +31,18 @@ server.post('/web-hook', function (req, response, next) {
             // do something with both responses
             const linkResponse = JSON.stringify(linkRes.data);
             const linkDetails = JSON.parse(linkResponse);
+            const image=[];
             for(let i=0; i<3; i++){
-                if(!linkDetails.items[i].pagemap.cse_thumbnail[0].src){
-                    linkDetails.items[i].pagemap.cse_thumbnail[0].src='https://hackernoon.com/drafts/unha26zu.png';
+                if(linkDetails.items[i].pagemap.cse_thumbnail[0].src !== undefined){
+                    image[i]= linkDetails.items[i].pagemap.cse_thumbnail[0].src;
+                }else{
+                    image[i]='https://hackernoon.com/drafts/unha26zu.png';
                 }
             }
             //linksData = linkDetails.items[0].link + "," + linkDetails.items[1].link + "," + linkDetails.items[2].link;
             linksData = linkDetails.items[0].link +'^'+ linkDetails.items[0].title + '^'+ linkDetails.items[0].snippet+"^"+linkDetails.items[0].pagemap.cse_thumbnail[0].src+"^"
                 +linkDetails.items[1].link+'^'+linkDetails.items[1].title+'^'+ linkDetails.items[1].snippet+"^"+linkDetails.items[1].pagemap.cse_thumbnail[0].src+"^"
-                +linkDetails.items[2].link +'^'+ linkDetails.items[2].title+'^'+ linkDetails.items[2].snippet+"^"+linkDetails.items[2].pagemap.cse_thumbnail[0].src;
+                +linkDetails.items[2].link +'^'+ linkDetails.items[2].title+'^'+ linkDetails.items[2].snippet+"^"+image[2];
             console.log('linkdata', linksData);
             const videoResponse = JSON.stringify(videoRes.data)
             const videoDetails = JSON.parse(videoResponse);
