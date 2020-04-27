31c7bc6a77dfb0cde057030c039ac231c0a28ab1
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-04-27 01:03:23 -0500

    'links'

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ server.post('/web-hook', function (req, response, next) {
 
     axios.all([
         axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&type=video&maxResults=3&order=relevance&relevanceLanguage=en&q=${query}&key=${apiKey}`),
-        axios.get(`https://www.googleapis.com/customsearch/v1?&key=${apiKey}&cx=017576662512468239146:omuauf_lfve&q=${query}&num=3&hl=en`)
+        axios.get(`https://www.googleapis.com/customsearch/v1?&key=${apiKey}&cx=014915153281259747060:rqmfryiuudy
+&q=${query}&num=3&hl=en`)
         // axios.get(videoUrl),
         // axios.get(linkUrl)
     ])
@@ -31,9 +32,12 @@ server.post('/web-hook', function (req, response, next) {
             // do something with both responses
             const linkResponse = JSON.stringify(linkRes.data);
             const linkDetails = JSON.parse(linkResponse);
-            linksData = linkDetails.items[0].link + "," + linkDetails.items[1].link + "," + linkDetails.items[2].link;
+            const link1 = [linkDetails.items[0].link, linkDetails.items[0].title, linkDetails.items[0].pagemap.cse_thumbnail[0].src];
+            const link2 = [linkDetails.items[1].link, linkDetails.items[1].title, linkDetails.items[1].pagemap.cse_thumbnail[0].src];
+            const link3 = [linkDetails.items[2].link, linkDetails.items[2].title, linkDetails.items[2].pagemap.cse_thumbnail[0].src];
+            linksData = link1 + "," + link2 + "," + link3;
             console.log('linkdata', linksData);
-            const videoResponse = JSON.stringify(videoRes.data)
+            const videoResponse = JSON.stringify(videoRes.data);
             const videoDetails = JSON.parse(videoResponse);
             let link = 'https://www.youtube.com/embed/';
             videoData = link + videoDetails.items[0].id.videoId + "," + link + videoDetails.items[1].id.videoId + "," + link + videoDetails.items[2].id.videoId;
