d5601f2b40eea5f25c3babc4138cd0356a45c4c6
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-04-30 19:56:30 -0500

    'quickreply'

--- index.js
+++ index.js
@@ -64,10 +64,12 @@ server.post('/web-hook', function (req, response, next) {
 
     if (action == 'level') {
         console.log("level intent");
+        console.log(query);
         axios.all([
             axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&type=video&maxResults=3&order=relevance&relevanceLanguage=en&q=${query}&key=${apiKey}`),
         ])
             .then(axios.spread((videoRes) => {
+                console.log(videoRes);
                 const videoResponse = JSON.stringify(videoRes);
                 const videoDetails = JSON.parse(videoResponse);
                 let link = 'https://www.youtube.com/embed/';
