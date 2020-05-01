03a21774ff29d3193f81ffbcc52b8ce95248f04a
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2020-04-30 20:37:29 -0500

    'quickreply'

--- index.js
+++ index.js
@@ -14,46 +14,82 @@ server.use(bodyParser.json());
 
 
 server.post('/web-hook', function (req, response, next) {
-    var linksData;
-    var videoData;
     const apiKeypooja = 'AIzaSyAE1FuSqmtiMvN_sh080MkV8ySFuiStwTU';
     const apiKeyMe = 'AIzaSyDhGASYUnmjszNIjzQ2Pr58YNc7xekWxWg';
     const apiKey = 'AIzaSyCQ9x6nIYd2dZDJj5crDkoopVBkDZbu4ws';
     const query = req.body.queryResult.queryText;
-    const videoUrl = encodeURI(`https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&type=video&maxResults=3&order=relevance&relevanceLanguage=en&q=${query}&key=${apiKey}`);
-    const linkUrl = encodeURI(`https://www.googleapis.com/customsearch/v1?&key=${apiKey}&cx=017576662512468239146:omuauf_lfve&q=${query}&num=3&hl=en`);
+    const action = req.body.queryResult.action;
+    if (action == 'video-intent') {
+        const intent = query;
+        const message = [{
+            quickReplies: {
+                title: 'What would you prefer?',
+                quickReplies: [
+                    'Watch Videos',
+                    'Read Tutorials'
+                ]
+            }
 
-    axios.all([
-        axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&type=video&maxResults=3&order=relevance&relevanceLanguage=en&q=${query}&key=${apiKey}`)
- ])
-        .then(axios.spread((videoRes, linkRes) => {
-            // do something with both responses
-            const videoResponse = JSON.stringify(videoRes.data);
-            const videoDetails = JSON.parse(videoResponse);
-            let link = 'https://www.youtube.com/embed/';
-            const messsage = [{
-                card: {
-                    imageUri: videoDetails.items[0].snippet.thumbnails.default.url,
-                    buttons: [
-                        {
-                            text: "Link1",
-                            postback: link + videoDetails.items[0].id.videoId
-                        }
-                    ]
-                }
-            }];
+        }];
 
-            return response.json({
-                fulfillmentText: link + videoDetails.items[0].id.videoId,
-                fulfillmentMessages: messsage,
-                speech: link + videoDetails.items[0].id.videoId,
-                source: 'get-Video-Details'
-            })
-                .catch(error => {
-                    console.log('heyehey', error);
-                });
+        return response.json({
+            fulfillmentMessages: message,
+            source: 'get-Video-Details'
+        })
+    }
 
-        }));
+    if (action == 'category') {
+        const category = query;
+        const message = [{
+            quickReplies: {
+                title: 'Tell us your expert level',
+                quickReplies: [
+                    'Beginner',
+                    'Intermediate',
+                    'Expert'
+                ]
+            }
+
+        }];
+
+        return response.json({
+            fulfillmentMessages: message,
+            source: 'get-category-details'
+        })
+    }
+
+    if (action == 'level') {
+        axios.all([
+            axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&type=video&maxResults=3&order=relevance&relevanceLanguage=en&q=${query}&key=${apiKey}`)
+        ])
+            .then(axios.spread((videoRes, linkRes) => {
+                const videoResponse = JSON.stringify(videoRes.data);
+                const videoDetails = JSON.parse(videoResponse);
+                let link = 'https://www.youtube.com/embed/';
+                const messsage = [{
+                    card: {
+                        imageUri: videoDetails.items[0].snippet.thumbnails.default.url,
+                        buttons: [
+                            {
+                                text: "Link1",
+                                postback: link + videoDetails.items[0].id.videoId
+                            }
+                        ]
+                    }
+                }];
+
+                return response.json({
+                    fulfillmentText: link + videoDetails.items[0].id.videoId,
+                    fulfillmentMessages: messsage,
+                    speech: link + videoDetails.items[0].id.videoId,
+                    source: 'get-Video-Details'
+                })
+                    .catch(error => {
+                        console.log('heyehey', error);
+                    });
+
+            }));
+    }
 });
 
 server.listen((process.env.PORT || 8333), () => {
